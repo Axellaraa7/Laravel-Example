54d40f21d3415da6c455e63e768c5dca93e10c05
Axel Lara <m.axellaraa@gmail.com>
2022-09-02 12:54:48 -0500

    A: Factories and seeders

--- app/Models/Ex1.php
+++ app/Models/Ex1.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,15 @@ class Ex1 extends Model
     use HasFactory;
 
     protected $table = 'ex1';
+
+    protected function name(): Attribute{
+      // return new Attribute(
+      //   get: fn($name) => ucwords($name),
+      //   set: fn($name) => strtoupper($name)
+      // );
+      return Attribute::make(
+        get: fn($name) => ucwords($name),
+        set: fn($name) => strtoupper($name)
+      );
+    }
 }
